56fe881a3b6e431fb9eaf29f490ed1456e83e0c8
Evgenii <shestakovevgeniy11@gmail.com>
2021-02-07 15:50:14 +0400

    добавил маршрутизацию

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
-import MainBoard from '../main-board/main-board';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import MainBoard from '../main-board/main-board';
+import MainLogin from '../login/login';
+import MainFavorites from '../favorites/favorites';
+import MainProperty from '../property/property';
+import PageNotFound from '../page-not-found/page-not-found';
+
 
 const App = (props) => {
   const {cardsCount} = props;
 
   return (
-    <MainBoard cardsCount={cardsCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainBoard cardsCount={cardsCount} />
+        </Route>
+        <Route exact path="/login">
+          <MainLogin/>
+        </Route>
+        <Route exact path="/favorites">
+          <MainFavorites />
+        </Route>
+        <Route exact path="/property/:id?">
+          <MainProperty />
+        </Route>
+        <Route>
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
